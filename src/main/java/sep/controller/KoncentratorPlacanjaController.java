482d8c8b12db3279c985e9991e9b0e62489577af
tttamara23 <tamara.perlinac@yahoo.com>
2018-11-15 19:33:03 +0100

    Placanje preko kartice

--- src/main/java/sep/controller/KoncentratorPlacanjaController.java
+++ src/main/java/sep/controller/KoncentratorPlacanjaController.java
@@ -1,14 +1,22 @@
 package sep.controller;
 
+import java.sql.Timestamp;
+import java.util.Random;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import sep.dto.MerchantDTO;
+import sep.dto.PaymentUrlIdDTO;
 
 @RestController
 @RequestMapping(value = "/zahtev")
@@ -21,6 +29,33 @@ public class KoncentratorPlacanjaController {
 	)
 	public ResponseEntity<?> posaljiZahtev(@RequestBody MerchantDTO merchant) {
 		System.out.println("Stao sam tu");
-		return new ResponseEntity<>(merchant, HttpStatus.OK);
+		//ovde treba koncentrator placanja da izgenerise merchantorderId, i to sve
+		Random randomGenerator = new Random();
+		merchant.setMerchant_order_id(randomGenerator.nextInt(1000));
+		//zakomentarisano jer puca na datetime kod onog slanja
+		/*Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+		merchant.setMerchant_timestamp(timestamp);
+		*/
+		
+		RestTemplate client = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        PaymentUrlIdDTO paymentUrlIdDTO = new PaymentUrlIdDTO();
+
+        try {
+
+            System.out.println("Prosledjujem zahtev banci prodavca");
+             //step 2
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            HttpEntity<MerchantDTO> entity = new HttpEntity<>(merchant, headers);
+            paymentUrlIdDTO = client.postForObject("http://localhost:1235/bank/proveriZahtev", entity,
+                    PaymentUrlIdDTO.class);
+            System.out.println("Vratila mi banka url: " + paymentUrlIdDTO.getUrl());
+            return new ResponseEntity<>(paymentUrlIdDTO, HttpStatus.OK);
+
+        } catch (Exception e) {
+            System.out.println("Ne moze da posalje");
+            return null; 
+        }
+		
 	}
 }
--- src/main/java/sep/dto/MerchantDTO.java
+++ src/main/java/sep/dto/MerchantDTO.java
@@ -1,6 +1,9 @@
 package sep.dto;
 
 import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+import org.joda.time.DateTime;
 
 public class MerchantDTO {
 	
@@ -9,6 +12,17 @@ public class MerchantDTO {
 	private String merchant_id;
 	
 	private String merchant_password;
+	
+	private Integer merchant_order_id;
+	
+	private Timestamp merchant_timestamp;
+	
+	private String successUrl;
+	
+	private String errorUrl;
+	
+	private String failedUrl;
+	
 	
 	public MerchantDTO() {
 		
@@ -36,6 +50,46 @@ public class MerchantDTO {
 
 	public void setMerchant_password(String merchant_password) {
 		this.merchant_password = merchant_password;
+	}
+
+	public Integer getMerchant_order_id() {
+		return merchant_order_id;
+	}
+
+	public void setMerchant_order_id(Integer merchant_order_id) {
+		this.merchant_order_id = merchant_order_id;
+	}
+
+	public Timestamp getMerchant_timestamp() {
+		return merchant_timestamp;
+	}
+
+	public void setMerchant_timestamp(Timestamp merchant_timestamp) {
+		this.merchant_timestamp = merchant_timestamp;
+	}
+
+	public String getSuccessUrl() {
+		return successUrl;
+	}
+
+	public void setSuccessUrl(String successUrl) {
+		this.successUrl = successUrl;
+	}
+
+	public String getErrorUrl() {
+		return errorUrl;
+	}
+
+	public void setErrorUrl(String errorUrl) {
+		this.errorUrl = errorUrl;
+	}
+
+	public String getFailedUrl() {
+		return failedUrl;
+	}
+
+	public void setFailedUrl(String failedUrl) {
+		this.failedUrl = failedUrl;
 	}
 	
 	
